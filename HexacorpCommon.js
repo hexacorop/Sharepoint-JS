b62bedcdb006244b089170fd46ada2e884139b80
hexacorop <59165177+hexacorop@users.noreply.github.com>
2019-12-24 13:14:53 +0530

    Update HexacorpCommon.js

--- HexacorpCommon.js
+++ HexacorpCommon.js
@@ -12,3 +12,33 @@
 
 
 
+//////////////////////////////
+Get Form field value in Display Form (DispForm.aspx):
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
+ 
+<script language="javascript" type="text/javascript">
+  
+  $(document).ready(function() {
+     var fldValue = $('h3:contains("Last Name")').closest('td').next('td').text();
+     alert(fldValue.trim());
+   });
+ 
+</script>
+
+
+/////////////////
+Get Form field value in Edit Form (EditForm.aspx):
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
+<script language="javascript" type="text/javascript">
+  
+  $(document).ready(function() {
+var fldValue = $("input[title*='Last Name']").val();
+alert(fldValue.trim());
+});
+ 
+</script>
+
+
+
