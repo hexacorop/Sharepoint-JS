ea6489e374ee77c997dc2bb9ebdbe65c35321a14
hexacorop <59165177+hexacorop@users.noreply.github.com>
2019-12-27 18:57:48 +0530

    Update HexacorpCommon.js

--- HexacorpCommon.js
+++ HexacorpCommon.js
@@ -65,6 +65,52 @@ var fldValue = $("input[title*='Last Name']").val();
 alert(fldValue.trim());
 });
  
+
+
+// Particlar person is deleted from the sharepoint group (sharepoint 2013 above code)
+             
+             var _PeoplePicker = $("div[title='columnname']");
+				//                _PeoplePicker[0].childElementCount
+             var getpeopleid  = _PeoplePicker[0].firstElementChild.getElementsByTagName("div")[0].getAttribute("key");
+             var user = getpeopleid.split("\\")[0] +"\\"+getpeopleid.split("\\")[1];
+
+    
+             var objperformcurrent = $("select[title='Performedby']").find("option:selected").text();
+             var removeUserUrl = "/_api/web/sitegroups/getbyname('"+objperformcurrent+"')/users/removeByLoginName";  
+             var metadata = {  
+             'loginName': user  
+             };  
+        
+             removeUser(removeUserUrl, metadata);
+
+
+    function removeUser(removeUserUrl, metadata) {  
+        $.ajax({  
+            url: _spPageContextInfo.webAbsoluteUrl + removeUserUrl,  
+            type: "POST",  
+            headers: {  
+                "accept": "application/json;odata=verbose",  
+                "X-RequestDigest": $("#__REQUESTDIGEST").val(),  
+                "content-Type": "application/json;odata=verbose"  
+            },  
+            data: JSON.stringify(metadata),  
+            success: function(data) {  
+               
+                console.log("User has been successfully removed from the SharePoint Group.");
+                
+        //var url = _spPageContextInfo.webAbsoluteUrl+'/Lists/UserPermission/AllItems.aspx';
+	//$(location).attr('href',url);
+  
+            },  
+            error: function(error) {  
+                alert(JSON.stringify(error));  
+            }  
+        });  
+    }  
+    
+
+
+
 </script>
 
 
